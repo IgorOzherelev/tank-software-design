1584dd1cdc42e788f92c1094deb5a9d78be3e8d6
IgorOzherelev <i9163073959@gmail.com>
2021-12-04 13:35:50 +0300

    [After lecture] исправления после лекции [2]

--- src/main/java/ru/mipt/bit/platformer/event/EventRemove.java
+++ src/main/java/ru/mipt/bit/platformer/event/EventRemove.java
@@ -2,14 +2,12 @@ package ru.mipt.bit.platformer.event;
 
 import ru.mipt.bit.platformer.graphics.objects.Drawable;
 import ru.mipt.bit.platformer.graphics.renderers.Renderer;
-import ru.mipt.bit.platformer.models.Colliding;
 import ru.mipt.bit.platformer.models.GameObject;
 
 import java.util.List;
-import java.util.Map;
 
 /**
- * Entity
+ * Adapter
  * */
 public class EventRemove implements Event {
     @Override
